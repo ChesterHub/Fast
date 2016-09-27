7716a13d5267d8a88f21dc37121781a24aea81c9
Chester Hoang <ChesterLHoang@gmail.com>
2016-09-27 12:26:28 -0700

    Add Unity Files
    Add unity Files for Roller Ball Game

---
+++ Assets/scripts/Accelerometer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Accelerometer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveX = Input.acceleration.x;
+        // transform.Rotate(temp, 0,0);
+        transform.Translate(moveX, 0, 0);
+        Debug.Log(moveX);
+	
+	}
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    private Rigidbody rb;
+    private int score;
+
+	// Use this for initialization
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        score = 0;
+        SetCountText();
+    }
+	// Update is called once per frame
+    void FixedUpdate() {
+        // float moveHorizontal = Input.GetAxis("Horizontal");
+        // float moveVertical = Input.GetAxis("Vertical");
+        // //transform.Translate(Input.acceleration.x, 0, -Input.acceleration.z);
+        // transform.Translate(Vector3.forward);
+
+        // Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        // rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickups")) {
+            other.gameObject.SetActive(false);
+            score += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + score.ToString();
+    }
+}
